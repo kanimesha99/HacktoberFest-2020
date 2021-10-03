5fb94c97fa23a1a6df7922c3e0925ce496c883a0
kanimesha99 <90630567+kanimesha99@users.noreply.github.com>
2021-10-03 06:49:15 -0700

    corrected the alignments

--- C/Bibyvarghese-poem.c
+++ C/Bibyvarghese-poem.c
@@ -4,9 +4,9 @@
 #include<stdio.h>
 int main()
 {
-printf("twinkle twinkle little star/n");
-printf(" how i wonder what you want/n");
-printf(" up above in the world of sky/n");
-printf("like a dimond in the sky/n");
-return0;
+printf("twinkle twinkle little star..\n");
+printf("how i wonder what you want..\n");
+printf("up above in the world of sky..\n");
+printf("like a dimond in the sky..\n");
+return 0;
 }
